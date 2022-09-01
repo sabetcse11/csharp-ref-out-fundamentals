57473607f662987fb35a4b2c9e2a1b3e009b655a
sabetcse11 <sabetcse11@gmail.com>
2022-09-01 10:44:12 +0600

    added project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TestClass
+    {
+        public static void GetNormalValue(int id)
+        {
+            id += 1;
+        }
+
+        public static void GetNormalValueOut(out int outPara)
+        {
+            outPara = 780000;
+
+            var newItem = outPara + outPara + outPara + 500;
+            return;
+        }
+
+        public static void GetNextName(ref int id)
+        {            
+
+            //insert
+            //update
+
+            //calculation
+            id =id+ 1;            
+        }
+
+        public static T GetQueryString<T>(string key, T defaultValue) 
+        { 
+        
+        }
+
+        static void Main(string[] args)
+        {
+            //normal
+            int k = 100;
+            GetNormalValue(k);
+            Console.WriteLine("Previous value of integer k:" + k.ToString());
+
+            //ref
+            int i = 1;
+            Console.WriteLine("Previous value of integer i:" + i.ToString());
+            GetNextName(ref i);
+            Console.WriteLine("Current value of integer i:" + i.ToString());
+
+            //out
+            int outParam=1000;
+            GetNormalValueOut(out outParam);
+
+            Console.WriteLine("Current value of integer i:" + outParam.ToString());
+        }
+    }
+  
+}
---
+++ ConsoleApp1/ConsoleApp1/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //namespace DoFactory.GangOfFour.Prototype.Structural
+    //{
+        #region Singleton
+        public class Singleton
+        {
+            private static Singleton instance;
+            public static Singleton GetInstance()
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                    return instance;
+                }
+                return instance;
+            }
+        }
+        public class Program
+        {
+            static void Main()
+            {
+                Singleton singleton = Singleton.GetInstance();
+                Singleton singleton2 = Singleton.GetInstance();
+                var hashCode1 = singleton.GetHashCode();
+                var hashCode2 = singleton.GetHashCode();
+                Console.WriteLine("hashCode1:{0}", hashCode1);
+                Console.WriteLine("hashCode1:{0}", hashCode2);
+                Console.ReadKey();
+            }
+        }
+        #endregion
+    }
+//}
